286f36e930afa36287b042e25521f32e2a50fa73
singtolee <liqiang0618@outlook.com>
2017-03-01 16:47:06 +0700

    add splash screen, need to resize src

---
+++ app/src/main/java/store/singto/singtostore/SplashScreen.java
@@ -0,0 +1,12 @@
+package store.singto.singtostore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
